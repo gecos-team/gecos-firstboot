9c46448d23c5a1ec00cb804d82e98d12c3888e4c
Antonio Hernández <ahernandez@emergya.com>
2011-10-11 11:38:59 +0200

    Move all galde files to ui files and create a catalog for each of them.

--- firstboot_lib/helpers.py
+++ firstboot_lib/helpers.py
@@ -35,14 +35,14 @@ from gettext import gettext as _
 gettext.textdomain('firstboot')
 
 def get_builder(builder_file_name):
-    """Return a fully-instantiated Gtk.Builder instance from specified ui 
+    """Return a fully-instantiated Gtk.Builder instance from specified ui
     file
-    
+
     :param builder_file_name: The name of the builder file, without extension.
         Assumed to be in the 'ui' directory under the data path.
     """
     # Look for the ui file that describes the user interface.
-    ui_filename = get_data_file('ui', '%s.glade' % (builder_file_name,))
+    ui_filename = get_data_file('ui', '%s.ui' % (builder_file_name,))
     if not os.path.exists(ui_filename):
         ui_filename = None
 
