14aa4f1b5bd10a162016665484f9954367f0c84a
David Amian <damian@emergya.com>
2011-12-14 13:00:07 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -114,7 +114,7 @@ class Clean(Command):
 
 DistUtilsExtra.auto.setup(
     name='firstboot',
-    version='0.3.2',
+    version='0.3.3',
     license='GPL-2',
     author='Antonio Hernández',
     author_email='ahernandez@emergya.com',
